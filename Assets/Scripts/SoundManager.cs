6ec53967ff231d3a2c6176befb77af81da9a391b
DevSooha <shua7288@ewhain.net>
2026-03-16 05:54:43 -0400

    SoundManager 제작
    roomManager 변경사항

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("BGM's")]
+    public AudioClip springBGM;
+    public AudioClip fallBGM;
+    
+
+    AudioSource audioSource;
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        // 중복 방지 + 씬 전환 시 파괴 안됨
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnSceneLoaded(RoomData roomData)
+    {        
+        if (roomData == null) return;
+        PlayBGMForScene(roomData.roomID);
+    }
+
+    void PlayBGMForScene(string roomID)
+    {
+        AudioClip clip = null;
+
+        switch (roomID)
+        {
+            case "aut_1" or "aut_2" or "aut_3": // 실제 씬 이름
+                clip = fallBGM;
+                break;
+            case "spr_1" or "spr_2" or "spr_3" or "spr_4" or "spr_5" or "spr_6" or "spr_7":
+                clip = springBGM;
+                break;
+        }
+        if (clip == null) return;
+        if (audioSource.clip == clip && audioSource.isPlaying)
+            return;
+
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.Play();  // AudioSource.Play로 재생[web:10]
+
+        Debug.Log("Playing music");
+        
+    }
+}
+
